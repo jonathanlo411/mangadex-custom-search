9f21d01920d1a7f5a24dca188b22352eee816647
Jonathan Lo <jonathanlo411@gmail.com>
2024-04-12 22:40:17 -0700

    feat: initialize with base

---
+++ search.py
@@ -0,0 +1,54 @@
+# Imports
+import requests as r
+from json import load
+
+# Globals
+BASE_URL = "https://api.mangadex.org"
+TAGS = r.get(
+    f"{BASE_URL}/manga/tag"
+).json()
+
+def main():
+    # Load tags
+    config = load(open('config.json', 'r'))
+    includes = obtain_tags(config['tags']['include'])
+    excludes = obtain_tags(config['tags']['exclude'])
+
+    # Make request to MangaDex
+    res = r.get(
+        f"{BASE_URL}/manga",
+        params={
+            "includedTags[]": includes,
+            "excludedTags[]": excludes,
+            "order[followedCount]": 'desc',
+            "limit": 100
+        }
+    )
+    clean = res.json()
+
+    # Validate
+    if (not clean) or (not 'data' in clean):
+        print('Error in request.')
+        return 0
+
+    # Ensure that there is a mal link
+    filtered = []
+    for manga in clean['data']:
+        links = manga['attributes']['links']
+        if links and 'mal' in links:
+            filtered.append(manga)
+
+    print(filtered)
+    
+
+
+def obtain_tags(tags: list[str]) -> list[str]:
+    return [
+        tag["id"]
+        for tag in TAGS["data"]
+        if tag["attributes"]["name"]["en"]
+        in tags
+    ]
+
+if __name__ == '__main__':
+    main()
